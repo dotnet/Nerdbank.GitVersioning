d177a384ca6095ddb142d9506780eeb5dbfa1951
Andrew Arnott <andarno@microsoft.com>
2015-05-16 21:37:41 -0700

    Dogfood our own package!

--- src/Nerdbank.GitVersioning.Tasks/Properties/AssemblyInfo.cs
+++ src/Nerdbank.GitVersioning.Tasks/Properties/AssemblyInfo.cs
@@ -21,16 +21,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("d76be1f9-5063-4443-b792-eaeecaa457d7")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
