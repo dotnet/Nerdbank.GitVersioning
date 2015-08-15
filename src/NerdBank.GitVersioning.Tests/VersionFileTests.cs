4a78a9b6610e27f184fb4850ca1c53db025b0fff
Sergey Shandar <seshanda@microsoft.com>
2015-08-14 18:48:24 -0700

    unused using are removed

--- src/NerdBank.GitVersioning.Tests/VersionFileTests.cs
+++ src/NerdBank.GitVersioning.Tests/VersionFileTests.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using LibGit2Sharp;
 using Nerdbank.GitVersioning;
 using Nerdbank.GitVersioning.Tests;
