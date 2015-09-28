881a2bc7d52d9011bc99d36b3f75d01d002aa305
Andrew Arnott <andrew.arnott@microsoft.com>
2015-09-27 20:33:33 -0700

    Update nupkg installation scripts for version.json

--- src/Nerdbank.GitVersioning.NuGet/tools/Install.ps1
+++ src/Nerdbank.GitVersioning.NuGet/tools/Install.ps1
@@ -1,11 +1,11 @@
 param($installPath, $toolsPath, $package, $project)
 
 $projectDirectory = Split-Path $project.FileName
-$generatedFile = .(Join-Path $toolsPath Create-VersionTxt.ps1) -ProjectDirectory $projectDirectory
+$generatedFile = .(Join-Path $toolsPath Create-VersionFile.ps1) -ProjectDirectory $projectDirectory
 
 if ($generatedFile)
 {
-    # Add version.txt to the project so it shows up in VS.
+    # Add version.json to the project so it shows up in VS.
     $result = $project.ProjectItems.AddFromFile($generatedFile)
 
     # By default, items get added with ItemType=Content, which could cause version.txt to be included in the build's output.
