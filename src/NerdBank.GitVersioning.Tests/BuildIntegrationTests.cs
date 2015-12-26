e09ac57b282e77ab07cbd9f93088dd11daf3612c
Andrew Arnott <andrewarnott@gmail.com>
2015-12-26 09:50:58 -0800

    Fix up test to work on delay-signed assemblies

--- src/NerdBank.GitVersioning.Tests/BuildIntegrationTests.cs
+++ src/NerdBank.GitVersioning.Tests/BuildIntegrationTests.cs
@@ -12,6 +12,9 @@ using LibGit2Sharp;
 using Microsoft.Build.Construction;
 using Microsoft.Build.Evaluation;
 using Microsoft.Build.Execution;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Nerdbank.GitVersioning;
 using Nerdbank.GitVersioning.Tests;
 using Validation;
@@ -387,20 +390,20 @@ public class BuildIntegrationTests : RepoTestBase
         this.testProject.AddProperty("DelaySign", delaySigned.ToString());
 
         this.WriteVersionFile();
-        var result = await this.BuildAsync("Build", logVerbosity: Microsoft.Build.Framework.LoggerVerbosity.Minimal);
-        string assemblyPath = result.BuildResult.ProjectStateAfterBuild.GetPropertyValue("TargetPath");
+        var result = await this.BuildAsync("GenerateAssemblyInfo", logVerbosity: Microsoft.Build.Framework.LoggerVerbosity.Minimal);
         string versionCsContent = File.ReadAllText(Path.Combine(this.projectDirectory, result.BuildResult.ProjectStateAfterBuild.GetPropertyValue("VersionSourceFile")));
         this.Logger.WriteLine(versionCsContent);
 
-        var assembly = Assembly.UnsafeLoadFrom(assemblyPath);
-        var thisAssemblyClass = assembly.GetType("ThisAssembly");
-        Assert.NotNull(thisAssemblyClass);
+        var sourceFile = CSharpSyntaxTree.ParseText(versionCsContent);
+        var syntaxTree = await sourceFile.GetRootAsync();
+        var fields = syntaxTree.DescendantNodes().OfType<VariableDeclaratorSyntax>();
 
-        const BindingFlags fieldFlags = BindingFlags.Static | BindingFlags.NonPublic;
+        var publicKeyField = (LiteralExpressionSyntax)fields.Single(f => f.Identifier.ValueText == "PublicKey").Initializer.Value;
+        var publicKeyTokenField = (LiteralExpressionSyntax)fields.Single(f => f.Identifier.ValueText == "PublicKeyToken").Initializer.Value;
         Assert.Equal(
             "002400000480000094000000060200000024000052534131000400000100010067cea773679e0ecc114b7e1d442466a90bf77c755811a0d3962a546ed716525b6508abf9f78df132ffd3fb75fe604b3961e39c52d5dfc0e6c1fb233cb4fb56b1a9e3141513b23bea2cd156cb2ef7744e59ba6b663d1f5b2f9449550352248068e85b61c68681a6103cad91b3bf7a4b50d2fabf97e1d97ac34db65b25b58cd0dc",
-            thisAssemblyClass.GetField("PublicKey", fieldFlags).GetValue(null));
-        Assert.Equal("ca2d1515679318f5", thisAssemblyClass.GetField("PublicKeyToken", fieldFlags).GetValue(null));
+            publicKeyField.Token.ValueText);
+        Assert.Equal("ca2d1515679318f5", publicKeyTokenField.Token.ValueText);
     }
 
     private void AssertStandardProperties(VersionOptions versionOptions, BuildResults buildResult, string relativeProjectDirectory = null)
