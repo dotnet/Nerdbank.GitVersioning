5e4d4778f2e13f7717fa58a5478b198d916c67be
Andrew Arnott <andrewarnott@gmail.com>
2016-06-13 04:36:20 -0700

    two

--- src/nerdbank-gitversioning.npm/gulpfile.js
+++ src/nerdbank-gitversioning.npm/gulpfile.js
@@ -5,8 +5,9 @@ var concat = require('gulp-concat');
 var sourcemaps = require('gulp-sourcemaps');
 var merge = require('merge2');
 var tslint = require('gulp-tslint');
+var del = require('del');
 
-const outDir = 'release';
+const outDir = 'out';
 var tsProject = ts.createProject('tsconfig.json', { declarationFiles: true });
 
 gulp.task('tsc', function() {
@@ -16,10 +17,10 @@ gulp.task('tsc', function() {
         .pipe(ts(tsProject));
 
     return merge([
-        tsResult.dts.pipe(gulp.dest(`{outDir}/definitions`)),
+        tsResult.dts.pipe(gulp.dest(`${outDir}/definitions`)),
         tsResult.js
             .pipe(sourcemaps.write('../maps'))
-            .pipe(gulp.dest(`{outDir}/js`))
+            .pipe(gulp.dest(`${outDir}/js`))
     ]);
 });
 
