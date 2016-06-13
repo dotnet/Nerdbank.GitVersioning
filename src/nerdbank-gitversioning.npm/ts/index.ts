a9ea6d097d9ed839f0a74cfa1a585c8270fe401a
Andrew Arnott <andrewarnott@gmail.com>
2016-06-13 09:29:31 -0700

    Obtain js object with all version details

--- src/nerdbank-gitversioning.npm/ts/index.ts
+++ src/nerdbank-gitversioning.npm/ts/index.ts
@@ -8,8 +8,16 @@ import {execAsync} from './asyncprocess';
 export async function getPackageVersion(projectDirectory: string) {
     var packageInstallPath = await installNuGetPackage('Nerdbank.GitVersioning', '1.4.41');
     var getVersionScriptPath = path.join(packageInstallPath.packageDir, "tools", "Get-Version.ps1");
-    var semver1 = await execAsync(`powershell -ExecutionPolicy Bypass -Command (${getVersionScriptPath} -ProjectDirectory "${projectDirectory}").SemVer1`)
-    return semver1.stdout;
+    var versionText = await execAsync(`powershell -ExecutionPolicy Bypass -Command (${getVersionScriptPath} -ProjectDirectory "${projectDirectory}")`)
+    var varsRegEx = /^(\w+)\s*: (.+)/mg;
+    var match;
+    var result = {};
+    while (match = varsRegEx.exec(versionText.stdout)) {
+        result[match[1]] = match[2];
+    }
+
+    //console.log(result);
+    return result;
 }
 
 getPackageVersion(process.env.npm_config_root || '.');
