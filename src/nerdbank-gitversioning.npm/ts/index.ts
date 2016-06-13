a7d7bfd794b49d5a25e47b543fe19baf1fbdb3ef
Andrew Arnott <andrewarnott@gmail.com>
2016-06-12 21:36:01 -0700

    Download nuget.exe

--- src/nerdbank-gitversioning.npm/ts/index.ts
+++ src/nerdbank-gitversioning.npm/ts/index.ts
@@ -1,16 +1,30 @@
 'use strict';
 
+import http = require('http');
 import * as q from 'q';
-const request = require('request');
-const fs = require('fs');
+import * as request from 'request';
+import * as fs from 'fs';
 
-function downloadNuGetExe(): q.Promise<string> {
-    console.log('Downloading nuget.exe...');
-    request('https://dist.nuget.org/win-x86-commandline/latest/nuget.exe')
-        .pipe(fs.createWriteStream('nuget.exe'));
-    console.log("Done");
-    return null;
-}
+async function existsAsync(path: string) {
+    return new Promise<boolean>(resolve => fs.exists(path, resolve));
+};
+
+export async function downloadNuGetExe(): Promise<string> {
+    const nugetExePath = 'nuget.exe';
+
+    if (!(await existsAsync(nugetExePath))) {
+        console.log('Downloading nuget.exe...');
+        var result = await new Promise<request.Request>(
+            (resolve, reject) => {
+                var req = request('https://dist.nuget.org/win-x86-commandline/latest/nuget.exe');
+                req.pipe(fs.createWriteStream(nugetExePath))
+                    .on('finish', () => resolve(req));
+            });
+        console.log('Download successful.');
+    }
+
+    return nugetExePath;
+};
 
 function getPackageVersion(): string {
     return "hi23";
