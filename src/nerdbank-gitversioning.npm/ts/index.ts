d84b2df520991b1681736b4786c601eb38d97a0a
Andrew Arnott <andrewarnott@gmail.com>
2016-06-12 22:13:33 -0700

    Install NB.GV in node script

--- src/nerdbank-gitversioning.npm/ts/index.ts
+++ src/nerdbank-gitversioning.npm/ts/index.ts
@@ -4,12 +4,24 @@ import http = require('http');
 import * as q from 'q';
 import * as request from 'request';
 import * as fs from 'fs';
+import {exec} from 'child_process';
 
-async function existsAsync(path: string) {
+function existsAsync(path: string) {
     return new Promise<boolean>(resolve => fs.exists(path, resolve));
 };
 
-export async function downloadNuGetExe(): Promise<string> {
+function execAsync(command: string) {
+    return new Promise<any>(
+        (resolve, reject) => exec(command, (error, stdout, stderr) => {
+            if (error) {
+                reject(error);
+            } else {
+                resolve({ stdout: stdout, stderr: stderr });
+            }
+        }));
+};
+
+async function downloadNuGetExe(): Promise<string> {
     const nugetExePath = 'nuget.exe';
 
     if (!(await existsAsync(nugetExePath))) {
@@ -25,8 +37,15 @@ export async function downloadNuGetExe(): Promise<string> {
     return nugetExePath;
 };
 
-function getPackageVersion(): string {
+async function installNuGetPackage(packageId: string) {
+    var nugetExePath = await downloadNuGetExe();
+    console.log(`Installing ${packageId}...`);
+    var result = await execAsync(`${nugetExePath} install ${packageId} -OutputDirectory .`);
+    console.log(result.stdout);
+};
+
+export async function getPackageVersion(): Promise<string> {
     return "hi23";
 }
 
-downloadNuGetExe();
+installNuGetPackage('Nerdbank.GitVersioning');
