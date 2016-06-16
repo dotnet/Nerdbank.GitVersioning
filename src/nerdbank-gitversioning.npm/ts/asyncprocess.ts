d86314b658349e4537025973aa10468145eff9ef
Andrew Arnott <andrewarnott@gmail.com>
2016-06-16 07:34:18 -0700

    Add interface for execAsync result

--- src/nerdbank-gitversioning.npm/ts/asyncprocess.ts
+++ src/nerdbank-gitversioning.npm/ts/asyncprocess.ts
@@ -1,7 +1,12 @@
 import {exec} from 'child_process';
 
+export interface IExecAsyncResult {
+    stdout: string;
+    stderr: string;
+}
+
 export function execAsync(command: string) {
-    return new Promise<any>(
+    return new Promise<IExecAsyncResult>(
         (resolve, reject) => exec(command, (error, stdout, stderr) => {
             if (error) {
                 reject(error);
