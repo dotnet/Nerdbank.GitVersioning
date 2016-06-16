09f43d5b18aa11cc4b0a30e50d6be52ea6a5867a
Andrew Arnott <andrewarnott@gmail.com>
2016-06-15 22:43:29 -0700

    Add doc comments and interface

--- src/nerdbank-gitversioning.npm/ts/index.ts
+++ src/nerdbank-gitversioning.npm/ts/index.ts
@@ -6,7 +6,26 @@ var camelCase = require('camel-case')
 import {installNuGetPackage} from './nuget'
 import {execAsync} from './asyncprocess';
 
-export async function getPackageVersion(projectDirectory: string) {
+/**
+ * The various aspects of a version that can be calculated.
+ */
+export interface IGitVersion {
+    version: string;
+    simpleVersion: string;
+    majorMinorVersoin: string;
+    commitId: string;
+    commitIdShort: string;
+    versionHeight: string;
+    semVer1: string;
+    semVer2: string;
+}
+
+/**
+ * Gets an object describing various aspects of the version of a project.
+ * @param projectDirectory The directory of the source code to get the version of.
+ */
+export async function getGitVersion(projectDirectory?: string) : Promise<IGitVersion> {
+    projectDirectory = projectDirectory || '.';
     var packageInstallPath = await installNuGetPackage('Nerdbank.GitVersioning', '1.4.41');
     var getVersionScriptPath = path.join(packageInstallPath.packageDir, "tools", "Get-Version.ps1");
     var versionText = await execAsync(`powershell -ExecutionPolicy Bypass -Command (${getVersionScriptPath} -ProjectDirectory "${projectDirectory}")`)
@@ -17,8 +36,21 @@ export async function getPackageVersion(projectDirectory: string) {
         result[camelCase(match[1])] = match[2];
     }
 
-    //console.log(result);
-    return result;
+    return <IGitVersion>result;
 }
 
-getPackageVersion(process.env.npm_config_root || '.');
+/**
+ * Sets an NPM package version based on the git height and version.json.
+ * @param packageDirectory The directory of the package about to be published.
+ * @param srcDirectory The directory of the source code behind the package, if different. 
+ */
+export async function setPackageVersion(packageDirectory?: string, srcDirectory?: string) {
+    packageDirectory = packageDirectory || '.';
+    srcDirectory = srcDirectory || packageDirectory;
+    const gitVersion = await getGitVersion(srcDirectory);
+    console.log(`Setting package version to ${gitVersion.semVer1}`);
+    var result = await execAsync(`npm version ${gitVersion.semVer1}`);
+    if (result.stderr) {
+        console.log(result.stderr);
+    }
+}
