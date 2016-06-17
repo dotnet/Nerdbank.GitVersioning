5d5bc965db5d518ac94ef93edcb44c5cc1fd62fc
Andrew Arnott <andrewarnott@gmail.com>
2016-06-16 21:25:36 -0700

    Reduce noise in package restore

--- init.ps1
+++ init.ps1
@@ -3,7 +3,7 @@ nuget restore "$PSScriptRoot\src" -Verbosity quiet
 
 Write-Host "Restoring NPM packages..." -ForegroundColor Yellow
 Push-Location "$PSScriptRoot\src\nerdbank-gitversioning.npm"
-npm install
+npm install --loglevel error
 
 Write-Host "Restoring Typings..." -ForegroundColor Yellow
  .\node_modules\.bin\typings install
