8b7d45fc9f997805b8382162a7f0825a4c757a6b
Andrew Arnott <andrewarnott@gmail.com>
2017-02-09 22:09:18 -0800

    Add real git submodule test
    And replace the reflection-based mini test.

---
+++ src/NerdBank.GitVersioning.Tests/VersionOracleTests.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+using LibGit2Sharp;
+using Nerdbank.GitVersioning;
+using Nerdbank.GitVersioning.Tests;
+using Xunit;
+using Xunit.Abstractions;
+
+public class VersionOracleTests : RepoTestBase
+{
+    public VersionOracleTests(ITestOutputHelper logger)
+        : base(logger)
+    {
+    }
+
+    [Fact]
+    public void Submodule_RecognizedWithCorrectVersion()
+    {
+        using (var expandedRepo = TestUtilities.ExtractRepoArchive("submodules"))
+        {
+            this.Repo = new Repository(expandedRepo.RepoPath);
+
+            var oracleA = VersionOracle.Create(Path.Combine(expandedRepo.RepoPath, "a"));
+            Assert.Equal("1.3.1", oracleA.SimpleVersion.ToString());
+            Assert.Equal("e238b03e75", oracleA.GitCommitIdShort);
+
+            var oracleB = VersionOracle.Create(Path.Combine(expandedRepo.RepoPath, "b", "projB"));
+            Assert.Equal("2.5.2", oracleB.SimpleVersion.ToString());
+            Assert.Equal("3ea7f010c3", oracleB.GitCommitIdShort);
+        }
+    }
+}
