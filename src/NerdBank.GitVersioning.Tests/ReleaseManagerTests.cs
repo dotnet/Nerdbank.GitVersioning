1e8611eb717125b1f62978f088caa0e2eabd94b5
Andreas Grünwald <andreasgruenwald@live.de>
2019-01-19 22:15:42 +0100

    Start implementing release creation

---
+++ src/NerdBank.GitVersioning.Tests/ReleaseManagerTests.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LibGit2Sharp;
+using Nerdbank.GitVersioning;
+using Nerdbank.GitVersioning.Tests;
+using Xunit;
+using Xunit.Abstractions;
+using static Nerdbank.GitVersioning.ReleaseManager;
+
+public class ReleaseManagerTests : RepoTestBase
+{
+    public ReleaseManagerTests(ITestOutputHelper logger) : base(logger)
+    {
+    }
+
+    [Fact]
+    public void PrepareRelease_NoGitRepo()
+    {
+        this.AssertError(
+            () => ReleaseManager.PrepareRelease(this.RepoPath),
+            ReleasePreparationError.NoGitRepo
+        );        
+    }
+
+    [Fact]
+    public void PrepareRelease_DirtyWorkingDirecotory()
+    {       
+        this.InitializeSourceControl();
+
+        File.WriteAllText(Path.Combine(this.RepoPath, "file1.txt"), "");
+
+        this.AssertError(
+            () => ReleaseManager.PrepareRelease(this.RepoPath),
+            ReleasePreparationError.UncommittedChanges
+        );  
+    }
+
+    [Fact]
+    public void PrepareRelease_DirtyIndex()
+    {
+        this.InitializeSourceControl();
+
+        var filePath = Path.Combine(this.RepoPath, "file1.txt");
+        File.WriteAllText(filePath, "");
+
+        Commands.Stage(this.Repo, filePath);
+
+
+        this.AssertError(
+            () => ReleaseManager.PrepareRelease(this.RepoPath),
+            ReleasePreparationError.UncommittedChanges
+        );
+    }
+
+    [Theory]
+    [InlineData(null)]
+    [InlineData("v{0}")]
+    public void PrepareRelease_OnReleaseBranch(string releaseBranchFormat)
+    {
+        var version = "1.2";
+        releaseBranchFormat = releaseBranchFormat ?? new VersionOptions.ReleaseOptions().BranchNameOrDefault;
+
+        this.InitializeSourceControl();
+        this.WriteVersionFile(new VersionOptions()
+        {
+            Version = new SemanticVersion(version),
+            Release = new VersionOptions.ReleaseOptions()
+            {
+                BranchName = releaseBranchFormat
+            }
+        });
+        
+
+        var branch = this.Repo.CreateBranch(String.Format(releaseBranchFormat, version));
+        Commands.Checkout(this.Repo, branch);
+
+
+        this.AssertError(
+            () => ReleaseManager.PrepareRelease(this.RepoPath),
+            ReleasePreparationError.OnReleaseBranch
+        );
+
+    }
+
+    [Fact]
+    //TODO: more test cases (different release settings)
+    public void PrepareRelease_OnMaster()
+    {
+        var version = SemanticVersion.Parse("1.2-pre");
+
+        // create and configure repository
+        this.InitializeSourceControl();
+        this.Repo.Config.Set("user.name", this.Signer.Name, ConfigurationLevel.Local);
+        this.Repo.Config.Set("user.email", this.Signer.Email, ConfigurationLevel.Local);
+        
+        // cretae version.json
+        var versionOptions = new VersionOptions()
+        {
+            Version = version,
+            Release = new VersionOptions.ReleaseOptions()
+        };
+        this.WriteVersionFile(versionOptions);
+
+        var expectedBranchName = String.Format(versionOptions.ReleaseOrDefault.BranchNameOrDefault, version.Version);
+
+        // prepare release
+        ReleaseManager.PrepareRelease(this.RepoPath);
+
+        // check if a branch was created
+        Assert.Contains(this.Repo.Branches, branch => branch.FriendlyName == expectedBranchName);
+        var masterBranch = this.Repo.Branches.Single(branch => branch.FriendlyName == "master");
+        var releaseBranch = this.Repo.Branches.Single(branch => branch.FriendlyName == expectedBranchName);
+
+        // check if if release branch contains a new commit
+        Assert.NotEqual(releaseBranch.Tip.Sha, masterBranch.Tip.Sha);
+
+        // check version on release branch
+        var releaseBranchVersion = VersionFile.GetVersion(releaseBranch.Tip);
+        Assert.Equal(version.Version.ToString(), releaseBranchVersion.Version.ToString());
+    }
+
+
+    private void AssertError(Action testCode, ReleasePreparationError expectedError)
+    {
+        var exception = Record.Exception(testCode);
+
+        Assert.NotNull(exception);
+        Assert.IsType<ReleasePreparationException>(exception);
+
+        Assert.Equal(expectedError, ((ReleasePreparationException)exception).Error);
+    }
+}
---
+++ src/NerdBank.GitVersioning/ReleaseManager.cs
@@ -0,0 +1,122 @@
+﻿namespace Nerdbank.GitVersioning
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+    using LibGit2Sharp;
+
+    /// <summary>
+    /// Methods for creating releases 
+    /// </summary>
+    public class ReleaseManager
+    {
+        /// <summary>
+        /// Defines the possible errors that can occur when preparing a release
+        /// </summary>
+        public enum ReleasePreparationError
+        {
+            /// <summary>
+            /// The project directory is not a git repository
+            /// </summary>
+            NoGitRepo,
+            /// <summary>
+            /// There are pending changes in the proejct directory
+            /// </summary>
+            UncommittedChanges,
+            /// <summary>
+            /// The current branch is already a release branch
+            /// </summary>
+            OnReleaseBranch
+        }
+
+        /// <summary>
+        /// Exception indicating an error during preparation of a release
+        /// </summary>
+        public class ReleasePreparationException : Exception
+        {
+            /// <summary>
+            /// Gets the error that occurred.
+            /// </summary>
+            public ReleasePreparationError Error { get; }
+
+            /// <summary>
+            /// Initializes a new instance ogf <see cref="ReleasePreparationException"/>
+            /// </summary>
+            /// <param name="error">The error that occurred.</param>
+            public ReleasePreparationException(ReleasePreparationError error) => this.Error = error;
+        }
+
+
+        /// <summary>
+        /// Prepares a release for the specified directory by creating a release branch and incrementing the version in the current branch.
+        /// </summary>
+        /// <exception cref="ReleasePreparationException">Thrown when the release could not be created.</exception>
+        //TODO: prerelease tag parameter
+        public static void PrepareRelease(string projectDirectory, TextWriter stdout = null, TextWriter stderr = null)
+        {
+            stdout = stdout ?? Console.Out;
+            stderr = stderr ?? Console.Error;
+            
+            // get the current version
+            var currentVersionOptions = VersionFile.GetVersion(projectDirectory);
+
+            // open the git repository
+            var repository = GitExtensions.OpenGitRepo(projectDirectory);
+            if(repository == null)
+            {
+                stderr.WriteLine($"No git repository found above directory '{projectDirectory}'");
+                throw new ReleasePreparationException(ReleasePreparationError.NoGitRepo);
+            }
+            
+            // abort if there are any pending changes
+            if(repository.RetrieveStatus().IsDirty)
+            {
+                stderr.WriteLine($"Uncommited changes in directory '{projectDirectory}'");
+                throw new ReleasePreparationException(ReleasePreparationError.UncommittedChanges);
+            }
+
+            var releaseBranchName = GetReleaseBranchName(currentVersionOptions);
+            var currentBranch = repository.Branches.Single(x => x.IsCurrentRepositoryHead);
+
+            // check if the current branch is the release branch
+            if (currentBranch.FriendlyName.Equals(releaseBranchName, StringComparison.OrdinalIgnoreCase))
+            {
+                //TODO: only update the version. For now, this is an error
+                throw new ReleasePreparationException(ReleasePreparationError.OnReleaseBranch);
+            }
+
+            // create release branch
+            
+            var releaseBranch = repository.CreateBranch(releaseBranchName);
+
+            // update version in release branch    
+            var releaseVersion = VersionFile.GetVersion(projectDirectory);
+            releaseVersion.Version = new SemanticVersion(releaseVersion.Version.Version);
+            Commands.Checkout(repository, releaseBranch);
+            var filePath = VersionFile.SetVersion(projectDirectory, releaseVersion);
+            Commands.Stage(repository, filePath);
+
+            var signature = repository.Config.BuildSignature(DateTimeOffset.Now);
+            if(signature == null)
+            {
+                //TODO
+            }
+            repository.Commit($"Create release branch for version '{releaseVersion.Version}'", signature, signature);
+
+            // switch back to previous branch
+            Commands.Checkout(repository, currentBranch);
+
+            //TODO: edit version on master current branch
+
+            //TODO: Merge back release branch 
+        }
+
+
+        private static string GetReleaseBranchName(VersionOptions versionOptions)
+        {
+            //TODO: Check format
+            return String.Format(versionOptions.ReleaseOrDefault.BranchNameOrDefault, versionOptions.Version.Version);                        
+        }
+
+    }
+}
