4dda3bb14009d8cc90da8e34c937d7732fa68163
Andreas Grünwald <andreasgruenwald@live.de>
2019-01-19 14:37:43 +0100

    Add SemanticVersion.Increment() extension method

---
+++ src/NerdBank.GitVersioning.Tests/SemanticVersionExtensionsTest.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+using Nerdbank.GitVersioning;
+using Xunit;
+using Xunit.Sdk;
+
+using static Nerdbank.GitVersioning.VersionOptions;
+
+/// <summary>
+/// Tests for <see cref=""/>
+/// </summary>
+public class SemanticVersionExtensionsTest
+{
+    /// <summary>
+    /// Provides testcases (original version, incremented version) in all supported version formats for
+    /// testing <see cref="SemanticVersionExtensions.Increment(SemanticVersion, ReleaseVersionIncrement)"/>
+    /// </summary>
+    public class TestDataAttribute : DataAttribute
+    {            
+        public override IEnumerable<object[]> GetData(MethodInfo testMethod)
+        {
+            // prefix: none or "v"
+            foreach (var prefix in new[] { "", "v" })
+            // version has either 2, 3 or 4 components
+            foreach (var precision in new[] { VersionPrecision.Minor, VersionPrecision.Build, VersionPrecision.Revision })
+            // version can have a prerelease tag
+            foreach (var prereleaseTag in new[] { "", "-pre", "-pre.{height}", "-{height}", "-{height}.build" }) 
+            // version can have build metadata
+            foreach (var buildMetadata in new[] { "", "+build", "+build.{height}", "+{height}", "+{height}.build" }) 
+            // either major or minor version can be incremented
+            foreach (var increment in new[] { ReleaseVersionIncrement.Major, ReleaseVersionIncrement.Minor }) 
+            {
+                var major = 1;
+                var minor = 2;
+                var build = 3;
+                var revision = 4;
+
+                var majorIncrement = increment == ReleaseVersionIncrement.Major ? 1 : 0;
+                var minorIncrement = increment == ReleaseVersionIncrement.Minor ? 1 : 0;
+
+                var current = $"{prefix}{major}.{minor}";
+                var next = $"{prefix}{major + majorIncrement}.{minor + minorIncrement}";
+
+                // append third and/or fourth version component
+                switch (precision)
+                {
+                    case VersionPrecision.Build:
+                        current += $".{build}";
+                        next += $".{build}";
+                        break;
+                    case VersionPrecision.Revision:
+                        current += $".{build}.{revision}";
+                        next += $".{build}.{revision}";
+                        break;
+                }
+
+                // append prerelease tag
+                current += prereleaseTag;
+                next += prereleaseTag;
+                
+                // append build metadata
+                current += buildMetadata;
+                next += buildMetadata;
+
+                yield return new object[]
+                {
+                    current,
+                    increment,
+                    next
+                };
+            }
+        }
+    }
+
+    [Theory]
+    [TestData]
+    public void IncrementVersion(string currentVersionString, ReleaseVersionIncrement increment, string expectedVersionString)
+    {
+        var currentVersion = SemanticVersion.Parse(currentVersionString);
+        var expectedVersion = SemanticVersion.Parse(expectedVersionString);
+
+        var actualVersion = currentVersion.Increment(increment);
+
+        Assert.Equal(expectedVersion, actualVersion);
+    }
+}
+
---
+++ src/NerdBank.GitVersioning/SemanticVersionExtensions.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Nerdbank.GitVersioning
+{
+    /// <summary>
+    /// Extension methods for <see cref="SemanticVersion"/>
+    /// </summary>
+    public static class SemanticVersionExtensions
+    {
+        /// <summary>
+        /// Gets a new semantic with the specified version component (major/minor) incremented.
+        /// </summary>
+        /// <param name="currentVersion">The version to increment.</param>
+        /// <param name="increment">Specifies whether to increment the major or minor version.</param>
+        /// <returns>Returns a new <see cref="SemanticVersion"/> object with either the major or minor version incremented by 1.</returns>
+        public static SemanticVersion Increment(this SemanticVersion currentVersion, VersionOptions.ReleaseVersionIncrement increment)
+        {
+            if (increment != VersionOptions.ReleaseVersionIncrement.Major && increment != VersionOptions.ReleaseVersionIncrement.Minor)
+                throw new ArgumentException($"Unexpected increment value '{increment}'", nameof(increment));
+
+            var majorIncrement = increment == VersionOptions.ReleaseVersionIncrement.Major ? 1 : 0;
+            var minorIncrement = increment == VersionOptions.ReleaseVersionIncrement.Minor ? 1 : 0;
+            
+            // use the appropriate constructor for the new version object
+            // dependening on whether the current versions has 2, 3 or 4 segements
+            Version newVersion;
+            if (currentVersion.Version.Build >= 0 && currentVersion.Version.Revision > 0)
+            {
+                // 4 segement version
+                newVersion = new Version(
+                    currentVersion.Version.Major + majorIncrement,
+                    currentVersion.Version.Minor + minorIncrement,
+                    currentVersion.Version.Build,
+                    currentVersion.Version.Revision);
+            }
+            else if (currentVersion.Version.Build >= 0)
+            {
+                // 3 segement version
+                newVersion = new Version(
+                    currentVersion.Version.Major + majorIncrement,
+                    currentVersion.Version.Minor + minorIncrement,
+                    currentVersion.Version.Build);
+            }
+            else
+            {
+                // 2 segment version
+                newVersion = new Version(
+                    currentVersion.Version.Major + majorIncrement,
+                    currentVersion.Version.Minor + minorIncrement);
+            }
+            
+            return new SemanticVersion(newVersion, currentVersion.Prerelease, currentVersion.BuildMetadata);
+        }
+    }
+}
