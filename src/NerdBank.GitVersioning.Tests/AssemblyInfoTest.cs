11540ab42ebfc39b1d2bf3e1d3945eef85e1cd05
JohannesWill <johannes.will@siemens.com>
2019-03-12 07:55:39 +0100

    Fix missing """

--- src/NerdBank.GitVersioning.Tests/AssemblyInfoTest.cs
+++ src/NerdBank.GitVersioning.Tests/AssemblyInfoTest.cs
@@ -36,7 +36,7 @@ namespace AssemblyInfo
 [<assembly: System.Reflection.AssemblyFileVersionAttribute(""1.3.1.0"")>]
 [<assembly: System.Reflection.AssemblyInformationalVersionAttribute("""")>]
 do()
-[<System.CodeDom.Compiler.GeneratedCode("" + AssemblyVersionInfo.GeneratorName + "","" + AssemblyVersionInfo.GeneratorVerison + "")>]
+[<System.CodeDom.Compiler.GeneratedCode(""" + AssemblyVersionInfo.GeneratorName + """,""" + AssemblyVersionInfo.GeneratorVerison + """)>]
 type internal ThisAssembly() =
   static member internal AssemblyVersion = ""1.3.0.0""
   static member internal AssemblyFileVersion = ""1.3.1.0""
