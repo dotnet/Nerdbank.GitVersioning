4540fe683501dd0fb2d13d699df867cb22da4311
Cameron Taggart <cameron.taggart@gmail.com>
2019-04-22 10:20:38 -0500

    pack to bin

--- build.ps1
+++ build.ps1
@@ -15,8 +15,9 @@ Param(
     [string]$MsBuildVerbosity = 'minimal'
 )
 
-$msbuildCommandLine = "dotnet build `"$PSScriptRoot\src\Nerdbank.GitVersioning.sln`" /m /verbosity:$MsBuildVerbosity /nologo /p:Platform=`"Any CPU`" /t:build,pack"
-$msbuildPublish = "dotnet publish .\src\nbgv\nbgv.csproj -f netcoreapp2.1 -o .\src\nerdbank-gitversioning.npm\out\nbgv.cli\tools\netcoreapp2.1\any"
+$msbuildCommandLine = "dotnet build `"$PSScriptRoot\src\Nerdbank.GitVersioning.sln`" /m /verbosity:$MsBuildVerbosity /nologo /p:Platform=`"Any CPU`""
+$msbuildPack = "dotnet pack --no-build `"$PSScriptRoot\src\Nerdbank.GitVersioning.sln`" -o bin"
+$msbuildPublish = "dotnet publish --no-build .\src\nbgv\nbgv.csproj -f netcoreapp2.1 -o .\src\nerdbank-gitversioning.npm\out\nbgv.cli\tools\netcoreapp2.1\any"
 
 if (Test-Path "C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll") {
     $msbuildCommandLine += " /logger:`"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll`""
@@ -24,6 +25,7 @@ if (Test-Path "C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll")
 
 if ($Configuration) {
     $msbuildCommandLine += " /p:Configuration=$Configuration"
+    $msbuildPack += " /p:Configuration=$Configuration"
     $msbuildPublish += " /p:Configuration=$Configuration"
 }
 
@@ -32,11 +34,18 @@ try {
     if ($PSCmdlet.ShouldProcess("$PSScriptRoot\src\Nerdbank.GitVersioning.sln", "msbuild")) {
         Invoke-Expression $msbuildCommandLine
         if ($LASTEXITCODE -ne 0) {
-            throw "MSBuild failed"
+            throw "dotnet build failed"
+        }
+        Invoke-Expression $msbuildPack
+        if ($LASTEXITCODE -ne 0) {
+            throw "dotnet pack failed"
+        }
+        Invoke-Expression $msbuildPublish
+        if ($LASTEXITCODE -ne 0) {
+            throw "dotnet publish failed"
         }
     }
-
-    Invoke-Expression $msbuildPublish
+    
     if ($PSCmdlet.ShouldProcess("$PSScriptRoot\src\nerdbank-gitversioning.npm", "gulp")) {
         cd "$PSScriptRoot\src\nerdbank-gitversioning.npm"
         yarn run build
