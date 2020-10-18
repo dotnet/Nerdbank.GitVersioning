1bb16dfac4f7962447dbd82d1cbe1ed0cc17d35a
Frederik Carlier <frederik.carlier@quamotion.mobi>
2020-10-18 14:49:04 +0200

    Add Benchmark project

---
+++ src/NerdBank.GitVersioning.Benchmarks/GetVersionBenchmarks.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using BenchmarkDotNet.Attributes;
+using Nerdbank.GitVersioning;
+
+namespace NerdBank.GitVersioning.Benchmarks
+{
+    class GetVersionBenchmarks
+    {
+        [Params(
+            "xunit;version.json",
+            "Cuemon;version.json",
+            "SuperSocket;version.json",
+            "NerdBank.GitVersioning;version.json")]
+        public string TestData;
+
+        public string RepositoryName => this.TestData.Split(';')[0];
+
+        public string VersionPath => this.TestData.Split(';')[1];
+
+        public string RepositoryPath => Path.Combine(
+                Environment.GetFolderPath(Environment.SpecialFolder.UserProfile),
+                @"Source\Repos",
+                this.RepositoryName);
+
+        public Version Version
+        { get; set; }
+
+        [Benchmark]
+        public void GetversionLibGit2()
+        {
+            var oracleA = VersionOracle.Create(this.RepositoryPath, Path.GetDirectoryName(this.VersionPath));
+            this.Version = oracleA.Version;
+        }
+    }
+}
---
+++ src/NerdBank.GitVersioning.Benchmarks/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using BenchmarkDotNet.Running;
+
+namespace NerdBank.GitVersioning.Benchmarks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<GetVersionBenchmarks>();
+        }
+    }
+}
