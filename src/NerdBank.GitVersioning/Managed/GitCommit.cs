82f431745b88d1f126debedfce5beb5c79d90697
Frederik Carlier <frederik.carlier@quamotion.mobi>
2020-10-19 22:58:10 +0200

    GitCommit: Add unit tests

--- src/NerdBank.GitVersioning/Managed/GitCommit.cs
+++ src/NerdBank.GitVersioning/Managed/GitCommit.cs
@@ -3,14 +3,33 @@ using System.Collections.Generic;
 
 namespace NerdBank.GitVersioning.Managed
 {
-    internal struct GitCommit : IEquatable<GitCommit>
+    /// <summary>
+    /// Represents a Git commit, as stored in the Git object database.
+    /// </summary>
+    public struct GitCommit : IEquatable<GitCommit>
     {
+        /// <summary>
+        /// Gets or sets the <see cref="GitObjectId"/> of the file tree which represents directory
+        /// structure of the repository at the time of the commit.
+        /// </summary>
         public GitObjectId Tree { get; set; }
+
+        /// <summary>
+        /// Gets or sets a <see cref="GitObjectId"/> which uniquely identifies the <see cref="GitCommit"/>.
+        /// </summary>
         public GitObjectId Sha { get; set; }
+
+        /// <summary>
+        /// Gets or sets a list of all parents of this commit.
+        /// </summary>
         public List<GitObjectId> Parents { get; set; }
 
+        /// <summary>
+        /// Gets or sets the author of this commit.
+        /// </summary>
         public GitSignature Author { get; set; }
 
+        /// <inheritdoc/>
         public override bool Equals(object obj)
         {
             if (obj is GitCommit)
@@ -21,26 +40,31 @@ namespace NerdBank.GitVersioning.Managed
             return false;
         }
 
+        /// <inheritdoc/>
         public bool Equals(GitCommit other)
         {
             return this.Sha.Equals(other.Sha);
         }
 
+        /// <inheritdoc/>
         public static bool operator ==(GitCommit left, GitCommit right)
         {
             return Equals(left, right);
         }
 
+        /// <inheritdoc/>
         public static bool operator !=(GitCommit left, GitCommit right)
         {
             return !Equals(left, right);
         }
 
+        /// <inheritdoc/>
         public override int GetHashCode()
         {
             return this.Sha.GetHashCode();
         }
 
+        /// <inheritdoc/>
         public override string ToString()
         {
             return $"Git Commit: {this.Sha}";
--- src/NerdBank.GitVersioning/Managed/GitSignature.cs
+++ src/NerdBank.GitVersioning/Managed/GitSignature.cs
@@ -2,10 +2,24 @@
 
 namespace NerdBank.GitVersioning.Managed
 {
-    struct GitSignature
+    /// <summary>
+    /// Represents the signature of a Git committer or author.
+    /// </summary>
+    public struct GitSignature
     {
+        /// <summary>
+        /// Gets or sets the name of the committer or author.
+        /// </summary>
         public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the e-mail address of the commiter or author.
+        /// </summary>
         public string Email { get; set; }
+
+        /// <summary>
+        /// Gets or sets the date and time at which the commit was made.
+        /// </summary>
         public DateTimeOffset Date { get; set; }
     }
 }
