4e4a916f8795043e893a5751143c9215a1ccdc91
Andrew Arnott <andrewarnott@live.com>
2022-05-12 13:53:28 -0600

    Fix tgz artifact capture

--- azure-pipelines/artifacts/deployables.ps1
+++ azure-pipelines/artifacts/deployables.ps1
@@ -5,9 +5,11 @@ if (!$BuildConfiguration) {
 }
 
 $PackagesRoot = "$RepoRoot/bin/Packages/$BuildConfiguration"
+$JsRoot = "$RepoRoot/bin/js"
 
 if (!(Test-Path $PackagesRoot))  { return }
 
 @{
-    "$PackagesRoot" = (Get-ChildItem $PackagesRoot -Recurse -Exclude *.LKG.*.nupkg)
+    "$PackagesRoot" = (Get-ChildItem $PackagesRoot -Recurse -Exclude *.LKG.*.nupkg);
+    "$JsRoot" = (Get-ChildItem $JsRoot *.tgz);
 }
