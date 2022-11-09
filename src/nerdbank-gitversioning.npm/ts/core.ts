8a00babfc9a7ca063ea143e5aba9cff7afd83371
Andrew Arnott <andrew.arnott@microsoft.com>
2022-11-09 14:00:23 -0700

    Drop .NET Core 3.1 support
    It falls out of its LTS support early next month.

--- src/nerdbank-gitversioning.npm/ts/core.ts
+++ src/nerdbank-gitversioning.npm/ts/core.ts
@@ -5,6 +5,6 @@ const nbgvPath = 'nbgv.cli';
 
 export function getNbgvCommand(dotnetCommand?: string): string {
     var command = dotnetCommand || 'dotnet';
-    const nbgvDll = path.join(__dirname, nbgvPath, "tools", "netcoreapp3.1", "any", "nbgv.dll");
+    const nbgvDll = path.join(__dirname, nbgvPath, "tools", "net6.0", "any", "nbgv.dll");
     return `${command} "${nbgvDll}"`;
 }
