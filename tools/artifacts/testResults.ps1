f6bf78a433cf283edcbc0ed24fb2aea8b79ccb73
Andrew Arnott <andrewarnott@live.com>
2024-12-31 19:59:11 -0700

    more logs

--- tools/artifacts/testResults.ps1
+++ tools/artifacts/testResults.ps1
@@ -7,9 +7,12 @@ $result = @{}
 $testRoot = Resolve-Path "$PSScriptRoot\..\..\test"
 $result[$testRoot] = (Get-ChildItem "$testRoot\TestResults" -Recurse -Directory | Get-ChildItem -Recurse -File)
 
+
 $artifactStaging = & "$PSScriptRoot\..\Get-ArtifactsStagingDirectory.ps1"
 $testlogsPath = Join-Path $artifactStaging "test_logs"
+Write-Host "Searching $testlogsPath for anything"
 if (Test-Path $testlogsPath) {
+    Write-Host "test log path exists"
     $result[$testlogsPath] = Get-ChildItem "$testlogsPath\*";
 }
 
