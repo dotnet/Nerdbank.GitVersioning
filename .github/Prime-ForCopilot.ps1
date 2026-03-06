41b6a219f210a07213e2c3d354c0c304378bbbc6
Andrew Arnott <andrew.arnott@microsoft.com>
2026-03-06 10:14:42 -0700

    Fix Prime-ForCopilot.ps1 when invoked from outside the repo

--- .github/Prime-ForCopilot.ps1
+++ .github/Prime-ForCopilot.ps1
@@ -1,4 +1,4 @@
-if ((git rev-parse --is-shallow-repository) -eq 'true')
+if ((git -C $PSScriptRoot rev-parse --is-shallow-repository) -eq 'true')
 {
     Write-Host "Shallow clone detected, disabling NBGV Git engine so the build can succeed."
     $env:NBGV_GitEngine='Disabled'
