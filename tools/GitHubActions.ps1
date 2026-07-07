90ac1ee05f361d25ff36e42ce217316768c2ee11
Andrew Arnott <andrew.arnott@microsoft.com>
2026-07-07 16:32:43 -0600

    Use preferred pattern for appending to PATH in GitHub Actions

--- tools/GitHubActions.ps1
+++ tools/GitHubActions.ps1
@@ -16,3 +16,18 @@ function Add-GitHubActionsEnvVariable {
     $delimiter = [guid]::NewGuid().ToString('N')
     [System.IO.File]::AppendAllText($Path, "$Name<<$delimiter`n$Value`n$delimiter`n", $utf8NoBom)
 }
+
+function Add-GitHubActionsPath {
+    param(
+        [string]$Path = $env:GITHUB_PATH,
+        [Parameter(Mandatory = $true)]
+        [string]$Value
+    )
+
+    if ([string]::IsNullOrWhiteSpace($Path)) {
+        throw "GitHub Actions path file must not be empty."
+    }
+
+    $utf8NoBom = [System.Text.UTF8Encoding]::new($false)
+    [System.IO.File]::AppendAllText($Path, "$Value`n", $utf8NoBom)
+}
