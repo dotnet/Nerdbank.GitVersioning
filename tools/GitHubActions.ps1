a4d4554915ecf1e181db41e2274969c21761d1f4
Andrew Arnott <andrew.arnott@microsoft.com>
2026-07-08 09:15:07 -0600

    Fix up error messages

--- tools/GitHubActions.ps1
+++ tools/GitHubActions.ps1
@@ -9,11 +9,11 @@ function Add-GitHubActionsEnvVariable {
     )
 
     if ([string]::IsNullOrWhiteSpace($Path)) {
-        throw "GitHub Actions environment file must not be empty."
+        throw "GitHub Actions environment file path must not be empty."
     }
 
     if ([string]::IsNullOrWhiteSpace($Name)) {
-        throw "GitHub Actions environment variable names must not be empty."
+        throw "GitHub Actions environment variable name must not be empty."
     }
 
     $utf8NoBom = [System.Text.UTF8Encoding]::new($false)
@@ -29,11 +29,11 @@ function Add-GitHubActionsPath {
     )
 
     if ([string]::IsNullOrWhiteSpace($Path)) {
-        throw "GitHub Actions path file must not be empty."
+        throw "GitHub Actions path file path must not be empty."
     }
 
     if ([string]::IsNullOrWhiteSpace($Value)) {
-        throw "GitHub Actions path entries must not be empty."
+        throw "GitHub Actions path entry must not be empty."
     }
 
     $utf8NoBom = [System.Text.UTF8Encoding]::new($false)
