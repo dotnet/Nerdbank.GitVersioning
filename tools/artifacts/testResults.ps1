abe5a3b63cdc254023208161484f94f4edb723f3
Andrew Arnott <andrew.arnott@microsoft.com>
2026-07-29 10:56:17 -0700

    Fix non-Windows path matching

--- tools/artifacts/testResults.ps1
+++ tools/artifacts/testResults.ps1
@@ -10,7 +10,7 @@ $legacyTestResults = Join-Path $testRoot TestResults
 if (Test-Path $legacyTestResults) {
     $result[$testRoot] = Get-ChildItem $legacyTestResults -Recurse -Directory |
         Get-ChildItem -Recurse -File |
-        Where-Object { $_.Extension -ne '.dmp' -or $_.FullName -match '\\In\\' }
+        Where-Object { $_.Extension -ne '.dmp' -or $_.FullName -match '[/\\]In[/\\]' }
 }
 
 $artifactStaging = & "$PSScriptRoot/../Get-ArtifactsStagingDirectory.ps1"
@@ -18,7 +18,7 @@ $testlogsPath = Join-Path $artifactStaging "test_logs"
 if (Test-Path $testlogsPath) {
     # Hang and crash dumps are copied into the TRX attachment directory.
     $result[$testlogsPath] = Get-ChildItem $testlogsPath -Recurse |
-        Where-Object { $_.Extension -ne '.dmp' -or $_.FullName -match '\\In\\' }
+        Where-Object { $_.Extension -ne '.dmp' -or $_.FullName -match '[/\\]In[/\\]' }
 }
 
 $result
